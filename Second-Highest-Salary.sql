71e3fb1b8ee201ac203c231e25bf53d69a2e0a29
Vinit <20949380+vinit0108@users.noreply.github.com>
2021-07-06 17:12:59 +0530

    Add files via upload

---
+++ Second-Highest-Salary.sql
@@ -0,0 +1,13 @@
+--SQL Query to find the 2nd highest salary in the Employee table
+
+Select Max(Salary) as Second Highest from Employees
+Where Salary < (Select Max(Salary) from Employees)
+
+
+/* Output
++---------------------+
+| Second Highest      |
++---------------------+
+| 3000                |
++---------------------+
+*/
